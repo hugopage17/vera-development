7390624ae772116c829866f7440a716715d5e218
Hugo Page <hpage@teamtalk.co.nz>
2020-04-21 19:23:01 +1200

    Updates

---
+++ precache-manifest.40eaf1e5d1224513143f0c2cd77d14be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b02c93d11b02330c9f7fc698a8249c",
+    "url": "/vera-development/index.html"
+  },
+  {
+    "revision": "7ca3dbb35b31f31c4118",
+    "url": "/vera-development/static/css/main.1ed4616f.chunk.css"
+  },
+  {
+    "revision": "82a3158c97408f264ad3",
+    "url": "/vera-development/static/js/2.dec48cb1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vera-development/static/js/2.dec48cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca3dbb35b31f31c4118",
+    "url": "/vera-development/static/js/main.1a4fee8f.chunk.js"
+  },
+  {
+    "revision": "90c26e4a4cfb921e10c1",
+    "url": "/vera-development/static/js/runtime-main.1e95bd25.js"
+  }
+]);
